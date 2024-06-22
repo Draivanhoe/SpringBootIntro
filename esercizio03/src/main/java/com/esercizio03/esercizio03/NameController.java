306790d29e2fad83e417b1b7ed3c8cba5f0ee0ec
Draivanhoe <115069225+Draivanhoe@users.noreply.github.com>
2024-06-22 19:46:24 +0200

    Add files via upload

---
+++ esercizio03/src/main/java/com/esercizio03/esercizio03/NameController.java
@@ -0,0 +1,12 @@
+package com.esercizio03.esercizio03;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+
+    @GetMapping(path = )
+    public String
+
+}
---
+++ esercizio03/src/test/java/com/esercizio03/esercizio03/Esercizio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esercizio03.esercizio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
