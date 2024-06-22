a6674ccfcfd02be34a089ce9a53f266e2062e856
Draivanhoe <115069225+Draivanhoe@users.noreply.github.com>
2024-06-22 19:43:48 +0200

    Add files via upload

---
+++ esercizio08/src/main/java/com/esercizio08/esercizio08/Students.java
@@ -0,0 +1,66 @@
+package com.esercizio08.esercizio08;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Students {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Students() {
+    }
+
+    public Students(String id, String lastName, String firstName, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ esercizio08/src/test/java/com/esercizio08/esercizio08/Esercizio08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esercizio08.esercizio08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
